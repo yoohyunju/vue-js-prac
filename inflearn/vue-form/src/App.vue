ad19b4036abf7a0ca80b28e798aa515bd2148c5f
yoohyunju <yhj2756@naver.com>
2023-05-26 18:16:19 +0900

    [12-1] 사용자 입력 폼 마크업

---
+++ inflearn/vue-form/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <form action="">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" />
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {
+  // 컴포넌트 단위로 개발할 때는 값의 참조가 일어나지 않게 function 단위로 data return
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<style></style>
